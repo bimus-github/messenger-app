374ba94d4e0ca0cfd4e246ed0aa58bd04e6edc41
bimus-github <bimus2022@gmail.com>
2024-02-06 15:57:45 +0500

    initial point

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "@/components/Themed";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const App = () => (
+  <SafeAreaView>
+    <View>
+      <Text>App</Text>
+    </View>
+  </SafeAreaView>
+);
+
+export default App;
