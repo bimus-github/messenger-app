b52e4b2ce222294e7a16e91941043d7308d03c7d
bimus-github <bimus2022@gmail.com>
2024-02-07 17:59:51 +0500

    fixing

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -2,10 +2,14 @@ import {
   Text as DefaultText,
   View as DefaultView,
   TouchableOpacity as DefaultTouchableOpacity,
+  Image as DefaultImage,
 } from "react-native";
+
+import PhoneInput from "react-native-phone-number-input";
 
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "./useColorScheme";
+import OTPInputView from "@twotalltotems/react-native-otp-input";
 
 type ThemeProps = {
   lightColor?: string;
@@ -15,7 +19,10 @@ type ThemeProps = {
 export type TextProps = ThemeProps & DefaultText["props"];
 export type ViewProps = ThemeProps & DefaultView["props"];
 export type TouchableOpacityProps = ThemeProps &
-  DefaultTouchableOpacity["props"];
+  DefaultTouchableOpacity["props"] & { text: string };
+export type IconProps = ThemeProps & DefaultImage["props"];
+export type PhoneNumberInputProps = ThemeProps & PhoneInput["props"];
+export type OTPInputViewProps = ThemeProps & OTPInputView["props"];
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
@@ -44,6 +51,7 @@ export function Text(props: TextProps) {
         {
           color: color,
           fontFamily: "Mulish",
+          fontSize: 16,
         },
         style,
       ]}
@@ -63,15 +71,82 @@ export function View(props: ViewProps) {
 }
 
 export function Button(props: TouchableOpacityProps) {
-  const { style, lightColor, darkColor, ...otherProps } = props;
+  const { style, lightColor, darkColor, text, ...otherProps } = props;
   const backgroundColor = useThemeColor(
     { light: lightColor, dark: darkColor },
     "bg_btn"
   );
+  const color = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "text_btn"
+  );
 
   return (
     <DefaultTouchableOpacity
-      style={[{ backgroundColor }, , style]}
+      style={[{ backgroundColor }, style]}
+      {...otherProps}
+    >
+      <DefaultText
+        style={[
+          { color, fontFamily: "Mulish", fontSize: 16, fontWeight: "700" },
+        ]}
+      >
+        {text}
+      </DefaultText>
+    </DefaultTouchableOpacity>
+  );
+}
+
+export function Icon(props: IconProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const color = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "text_main"
+  );
+
+  return <DefaultImage style={[{ tintColor: color }, style]} {...otherProps} />;
+}
+
+export function PhoneNumberInput(props: PhoneNumberInputProps) {
+  const colorScheme = useColorScheme();
+  const { lightColor, darkColor, ...otherProps } = props;
+  const color = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "text_placeholder"
+  );
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "bg_input"
+  );
+  return (
+    <PhoneInput
+      textInputStyle={[{ color, backgroundColor }]}
+      textContainerStyle={[{ backgroundColor }]}
+      codeTextStyle={[{ color }]}
+      containerStyle={[{ backgroundColor }]}
+      countryPickerButtonStyle={[{ backgroundColor }]}
+      {...otherProps}
+      withDarkTheme={colorScheme === "dark"}
+    />
+  );
+}
+
+export function OTPInput(props: OTPInputViewProps) {
+  const { lightColor, darkColor, ...otherProps } = props;
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "bg_input"
+  );
+  const color = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "text_placeholder"
+  );
+  return (
+    <OTPInputView
+      style={{ width: "100%", height: 100 }}
+      codeInputHighlightStyle={{ backgroundColor, borderRadius: 100, color }}
+      codeInputFieldStyle={{ backgroundColor, borderRadius: 100, color }}
+      placeholderTextColor={color}
       {...otherProps}
     />
   );
