9d081afbaf615cb9841c354877af3521fa963f0a
bimus-github <bimus2022@gmail.com>
2024-02-07 20:16:05 +0500

    fixing

--- app/auth/AuthWithPhoneNumber.tsx
+++ app/auth/AuthWithPhoneNumber.tsx
@@ -1,21 +1,35 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import React from "react";
-import {
-  Button,
-  Icon,
-  PhoneNumberInput,
-  Text,
-  View,
-} from "@/components/Themed";
-import { chevron_left } from "@/constants/icons";
+import { StyleSheet } from "react-native";
+import React, { useRef, useState } from "react";
+import { Button, PhoneNumberInput, Text, View } from "@/components/Themed";
 import { useRouter } from "expo-router";
 import Gap from "@/components/Gap";
 import { sreenHeight, sreenWidth } from "@/constants/Dimensions";
 import Header from "@/components/auth/Header";
 
+import { FirebaseRecaptchaVerifierModal } from "expo-firebase-recaptcha";
+import { app, auth } from "@/firebase";
+import { signInWithPhoneNumber } from "firebase/auth";
+
 const AuthWithPhoneNumber = () => {
+  const recaptchaVerifier = useRef(null);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [phoneCode, setPhoneCode] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   const router = useRouter();
+
+  const handleContinue = async () => {
+    const phone = `+${phoneCode}${phoneNumber}`;
+    setIsLoading(true);
+    const result = await signInWithPhoneNumber(
+      auth,
+      phoneNumber,
+      recaptchaVerifier.current as any
+    );
+    setIsLoading(false);
+    console.log(result);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
@@ -27,16 +41,18 @@ const AuthWithPhoneNumber = () => {
           Please confirm your country code and enter your phone number
         </Text>
         <Gap vertical={sreenHeight * 0.15} />
-        <PhoneNumberInput containerStyle={{ width: "100%" }} />
+        <FirebaseRecaptchaVerifierModal
+          ref={recaptchaVerifier}
+          firebaseConfig={app.options}
+        />
+        <PhoneNumberInput
+          onChangeText={(text) => setPhoneNumber(text)}
+          onChangeCountry={(text) => setPhoneCode(text.callingCode[0])}
+          containerStyle={{ width: "100%" }}
+        />
         <Gap vertical={sreenHeight * 0.3} />
         <View style={{ alignItems: "center" }}>
-          <Button
-            onPress={() => {
-              router.push("/auth/CheckingNumber");
-            }}
-            text="Get Started"
-            style={styles.btn}
-          />
+          <Button onPress={handleContinue} text="Continue" style={styles.btn} />
         </View>
       </View>
     </SafeAreaView>
