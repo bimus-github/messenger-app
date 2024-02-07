3bd3ee9b754ba8d7dc9a79c0485bdcf45a28123e
bimus-github <bimus2022@gmail.com>
2024-02-07 20:16:07 +0500

    fixing

---
+++ firebase/index.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import "@firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAG5rqJUgNRdRca9tKzVmm-UfpxHgWzftg",
+  authDomain: "messenger-app-7f9f4.firebaseapp.com",
+  projectId: "messenger-app-7f9f4",
+  storageBucket: "messenger-app-7f9f4.appspot.com",
+  messagingSenderId: "71307836965",
+  appId: "1:71307836965:web:8790d123bf2ef2defd09cc",
+  measurementId: "G-1RJJ8GY2WW",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, app };
