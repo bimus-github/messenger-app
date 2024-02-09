fad035fccb98cc65a3cf125a161abb4f2d4bfcf4
bimus-github <bimus2022@gmail.com>
2024-02-09 16:01:26 +0500

    fixing

--- models/type.ts
+++ models/type.ts
@@ -3,3 +3,17 @@ export enum Checking_Account_Status {
   SIGNED_IN = "SIGNED_IN",
   LOGED_OUT = "LOGED_OUT",
 }
+
+export type User_Type = {
+  id: string;
+  fName: string;
+  lName: string;
+  phone: string;
+  image: string;
+  status: User_Status;
+};
+
+export enum User_Status {
+  ONLINE = "ONLINE",
+  OFFLINE = "OFFLINE",
+}
